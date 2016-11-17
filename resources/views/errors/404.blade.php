e55465179a002529e83e1bab4f673431601db766
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-11-16 21:51:53 -0500

    Page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>II Full Day de Gestión de TI | UNT</title>
+        <!-- for-mobile-apps -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Design UNT" />
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+            function hideURLbar(){ window.scrollTo(0,1); } </script>
+        <!-- //for-mobile-apps -->
+        <link href="{{ asset('404/css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+        <link href='//fonts.googleapis.com/css?family=Viga' rel='stylesheet' type='text/css'>
+    </head>
+<body>
+    <div class="main">
+        <div class="nav_w3l">
+            <ul>
+                <li class="active"><a href="{{ url('/') }}">Inicio</a></li>
+                <li><a href="{{ url('/') }}#whatis" class="hvr-sweep-to-bottom">¿Qué es?</a></li>
+                <li><a href="{{ url('/') }}#useit" class="hvr-sweep-to-bottom">Ponentes</a></li>
+                <li><a href="{{ url('/') }}#screen" class="hvr-sweep-to-bottom">Ponencias</a></li>
+                <li><a href="{{ url('/') }}#news" class="hvr-sweep-to-bottom">Novedades</a></li>
+                <li><a href="{{ url('/') }}#credits" class="hvr-sweep-to-bottom">Itinerario</a></li>
+                <li><a href="{{ url('/') }}#sponsors" class="hvr-sweep-to-bottom">Auspiciadores</a></li>
+            </ul>
+        </div>
+        <h1>404</h1>
+        <h2>Lo sentimos, página no encontrada.</h2>
+        <div class="more_w3ls">
+            <a href="">Intentar otra vez</a>
+        </div>
+        <div class="wthree_social_icons">
+            <div>
+                <a target="_blank" href="https://www.facebook.com/II-Full-Day-de-Gesti%C3%B3n-de-TI-188251811510916/?fref=nf"><span>II Full Day Gestión de TI</span></a>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© 2016 II Full Day de Gestión de TI | UNT. All rights reserved | Design by <a href="#">EnigmaticTeam</a></p>
+        </div>
+    </div>
+</body>
+</html>
