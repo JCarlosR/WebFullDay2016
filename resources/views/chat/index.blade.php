7bdc71a699a45e64f1be3c73a3b38cef4c4fe3ee
Juan Ramos <juancagb.17@gmail.com>
2016-11-26 09:46:46 -0500

    Changes UI

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.panel')
 
-@section('title','Chat Realtime')
+@section('title','Preguntas en Realtime')
 
 @section('manage-chat','active')
 
@@ -9,7 +9,7 @@
         <i class="ace-icon fa fa-home home-icon"></i>
         <a href="#">Chat</a>
     </li>
-    <li class="active">Chat Realtime with Firebase</li>
+    <li class="active">Preguntas en Realtime</li>
 @endsection
 
 @section('styles')
@@ -55,17 +55,11 @@
 @section('content')
     <div class="container-fluid">
         <div class="row" id="chatUl">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-sm-6 col-md-offset-3">
-
-                    </div>
-                </div>
-            </div>
-
+            <div class="col-md-6 col-md-offset-3">
         </div>
     </div>
 @endsection
+
 @section('scripts')
     <script src="https://www.gstatic.com/firebasejs/3.6.0/firebase.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -85,50 +79,6 @@
         var $txtPregunta = $('#pregunta');
         var $btnEnviar = $('#btnEnviar');
         var $chatUl = $('#chatUl');
-
-        $(document).on('click', '[data-question]', function () {
-            var id = $txtId.val();
-            var key = $(this).data('question');
-            var likes = $(this).data('likes');
-
-            firebase.database().ref('likes/'+id+'/'+key).once('value', function(snap) {
-                if (snap.val() == null) {
-                    var questionLikeRef = firebase.database().ref('questions/'+key+'/likes');
-                    questionLikeRef.transaction(function(currentData) {
-                        questionLikeRef.set(likes+1);
-
-                    }, function(error, committed, snapshot) {
-                        if (error) {
-                            console.log('Transaction failed abnormally!', error);
-                        } else if (!committed) {
-                            console.log('We aborted the transaction (because ada already exists).');
-                        } else {
-                            console.log('User ada added!');
-                        }
-                        console.log("Ada's data: ", snapshot.val());
-                    });
-                    firebase.database().ref('likes/'+id+'/'+key).set(true);
-                } else {
-                    var questionLikeRef = firebase.database().ref('questions/'+key+'/likes');
-                    questionLikeRef.transaction(function(currentData) {
-                        if (currentData != null) {
-                            questionLikeRef.set(likes-1);
-                            firebase.database().ref('likes/'+id+'/'+key).remove();
-                        }
-                    }, function(error, committed, snapshot) {
-                        if (error) {
-                            console.log('Transaction failed abnormally!', error);
-                        } else if (!committed) {
-                            console.log('We aborted the transaction (because ada already exists).');
-                        } else {
-                            console.log('User ada added!');
-                        }
-                        console.log("Ada's data: ", snapshot.val());
-                    });
-                }
-            });
-        });
-
 
         $btnEnviar.on("click", function() {
             var id = $txtId.val();
@@ -153,10 +103,9 @@
                         var likes = element.likes;
                         var description = element.description;
                         var li_html = '';
-                        li_html += '<div class="container">';
                         li_html += '    <div class="row">';
                         li_html += '        <div class="col-md-8 col-sm-8 col-md-offset-2">';
-                        li_html += '            <div class="card-style" >';
+                        li_html += '            <div class="card-style">';
                         li_html += '                <div class="media">';
                         li_html += '                    <div class="media-left">';
                         li_html += '                        <img class="media-object img-thumbnail card-img" src="{{ asset('images/logo_200_200.png') }}">';
@@ -164,35 +113,17 @@
                         li_html += '                    <div class="media-body">';
                         li_html += '                        <p><h5 class="media-heading">'+nombre+'</h5></p>';
                         li_html += '                        <p class="description">'+description+'</p>';
-                        li_html += '                        <p class="members">A     </p><p class="badge">'+likes+'</p><p class="members">personas le gusta esta pregunta.</p>';
+                        li_html += '                        <p class="members">A </p><p class="badge">'+likes+'</p><p class="members">personas le gusta esta pregunta.</p>';
                         li_html += '                    </div>';
                         li_html += '                </div>';
                         li_html += '            </div>';
                         li_html += '        </div>';
                         li_html += '    </div>';
-                        li_html += '</div>';
                         html = li_html + html;
 
 
                     });
                     $chatUl.html(html);
-                    putTextIntoButton();
                 });
-
-        function putTextIntoButton() {
-            $buttons = $('[data-question]');
-            $buttons.each(function (i, button) {
-                var $button = $(button);
-                var key = $button.data('question');
-                var id = $txtId.val();
-                firebase.database().ref('likes/'+id+'/'+key).once('value', function(snap) {
-                    if (snap.val() == null)
-                        $button.text('Me  gusta');
-                    else
-                        $button.text('Ya no me  gusta');
-                    $button.show();
-                });
-            });
-        }
     </script>
 @endsection
